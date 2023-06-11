5ab0ae3582e5af87b96838e5064eb1fda376f5f0
TimPavlov <nayaroka@yandex.by>
2023-06-11 14:32:08 +0300

    pull from db, add create form and html view

--- les_16/les_16/settings.py
+++ les_16/les_16/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todos',
-    'django-extensions',
 ]
 
 MIDDLEWARE = [
@@ -123,4 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-URL = "https://jsonplaceholder.typicode.com/todos"
--- les_16/todos/views.py
+++ les_16/todos/views.py
@@ -1,83 +1,40 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-import requests
-from django.conf import settings
+from .models import Todo
 
 
-class Todos:
-    def __init__(self, todo=None):
-        self.todo = todo or []
+"""Если снова делать migrate. ТО!!! комитить эти строки"""
+todos = Todo.objects.all()
+todos_data = [todo for todo in todos.values('id', 'name', 'description', 'completed')]
 
-    def __iter__(self):
-        self.i = -1
-        self.count = len(self.todo)
-        return self
-
-    def __next__(self):
-        if self.i < self.count:
-            self.i += 1
-            return self.todo[self.i]
-        raise StopIteration
-
-    def __getitem__(self, item):
-        return self.todo[item]
-
-    def append_todo(self, todo_):
-        self.todo.append(todo_)
-
-    def get_by_id(self, id):
-        return next((i for i in self.todo if i.id == id), None)
-
-    def output_in_json(self):
-        json_list = []
-        for i in self.todo:
-            json_list.append(i.for_json_out())
-        return json_list
-
-class Todo:
-    def __init__(self, userId, id, title, completed):
-        self.userId = userId
-        self.id = id
-        self.title = title
-        self.completed = completed
-
-    def for_json_out(self):
-        return {
-            "userId": self.userId,
-            "id": self.id,
-            "title": self.title,
-            "completed": self.completed
-        }
-
-
-todos = Todos()
-for i in requests.get(settings.URL).json():
-    todos.append_todo(Todo(i['userId'], i['id'], i['title'], i['completed']))
-
-def todo_db(request):
-    from .models import Todo
-    todos = Todo.objects.all()
-    todos_data = [todo for todo in todos.values('name','description', 'completed')]
+def todo_json(request):
     return JsonResponse({'todos': todos_data})
 
-def json_out(request):
-    return JsonResponse({'todos': todos.output_in_json()})
+"""Возможность сохранять новый todo сразу в базу. Но так как пока нету красивой привязки"""
+"""к user'у в create-форме(через Имя например) то реализовано на костыле(через user_1)"""
+def todo_save(todo_):
+    Todo(
+        id=todo_["id"],
+        name=todo_["name"],
+        description=todo_["description"],
+        user_id=1
+    ).save()
 
 def home(request):
     if request.method == "GET":
-        return render(request, 'todos.html', {'todos': todos.output_in_json()})
+        return render(request, 'todos.html', {'todos': todos_data})
     elif request.method == "POST":
         todo = dict(request.POST)
         for key in todo:
             todo[key] = todo[key][0]
-        todo["id"] = int(todo["id"])
-        todo["userId"] = int(todo["userId"])
-        todos.append_todo(Todo(todo['userId'], todo['id'],
-                               todo['title'], todo['completed']))
-        return render(request, "todos.html", {"todos": todos.output_in_json()})
+        todo["id"] = len(todos_data)+1
+        todos_data.append({"id": todo["id"], "name": todo["name"],
+                           "description": todo["description"], "completed": False})
+#       todo_save(todo)
+        return render(request, "todos.html", {"todos": todos_data})
 
 def get_todo(request, id):
-    return render(request, 'todos.html', {'todos': [todos.get_by_id(id).for_json_out()]})
+    return render(request, 'todos.html', {'todos': [todos_data[id-1]]})
 
 def create_todo(request):
     return render(request, "create_todo.html")
