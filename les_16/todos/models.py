5e4b567505bc52a38d9a65ab698940ddcfd3e00d
PavlovTim <129174224+PavlovTim@users.noreply.github.com>
2023-06-18 11:59:42 +0300

    Delete les_16 directory

--- les_16/todos/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-
-class Todo(models.Model):
-    name = models.CharField(max_length=128)
-    description = models.CharField(max_length=255)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    completed = models.BooleanField(default=False)
--- les_16/todos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.todo_json),
-    path('home', views.home, name='home'),
-    path('<int:id>', views.get_todo),
-    path('create', views.create_todo),
-]
